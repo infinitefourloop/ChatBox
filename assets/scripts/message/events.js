e7d695278ce0dfe2d96f176bed7165083b52333c
brettdoyle44 <brettdoyle44@gmail.com>
2019-08-09 09:57:15 -0400

    Initial edit functionality
    - Add edit API and Events

--- assets/scripts/message/events.js
+++ assets/scripts/message/events.js
@@ -4,6 +4,7 @@ const io = require('socket.io-client')
 const socket = io.connect('localhost:4741')
 const api = require('./api')
 const ui = require('./ui')
+const getFormFields = require('../../../lib/get-form-fields')
 
 const onSend = function (event) {
   event.preventDefault() // prevents page reloading
@@ -32,6 +33,17 @@ const onDeleteMessage = (event) => {
       socket.emit('delete message', response)
     })
     .catch(ui.deleteMessageFailure)
+}
+
+const onEditMessage = (event) => {
+  event.preventDefault()
+  const form = event.target
+  const formData = getFormFields(form)
+  const id = $(event.target).data('id')
+  api.editMessage(formData, id)
+    .then(api.indexMessages)
+    .then(ui.indexMessagesSuccess)
+    .catch(ui.editMessageFailure)
 }
 
 const addHandlers = () => {
