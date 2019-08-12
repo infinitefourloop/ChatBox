c76f707352e4904ead3062c9dc2890665999d568
ekmy318 <eunice.xp@gmail.com>
2019-08-12 12:17:32 -0400

    remove slash in config
    Co-Authored-By: elody-ramirez <elody-ramirez@users.noreply.github.com>

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://young-springs-61213.herokuapp.com/',
+  production: 'https://young-springs-61213.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
